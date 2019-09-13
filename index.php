c601dd8e1c5c57637d16868318a3c5fae6e0bf79
piyawat9547 <55186281+piyawat9547@users.noreply.github.com>
2019-09-13 22:14:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <?php
-
-$access_token = 'gdfWCjyBwS+xb/EusK4cvcTATrcIgs1bK6kkU1cx/Bhi3Tta9n08ZPt/tFPS7iWFLIWMxLYckZaGVeXnrpOCxnK/5FkG+Vp6tadAk2DBLE29Ej0VrSgDFuHiOM16Qf2N2TPGvEu3iXHdOt62LUqLAAdB04t89/1O/w1cDnyilFU=';
+$access_token = '5gZQWeN7r4W76y0rDoTq1kmZKNe0AHqZCoN0qKKUpVRyTg1qYcDk+9uvFzT0wOC1T6YhxwQ6qdRd7ld6Nnf/VT6rhFuPKAXakQ2gQazw/rDdeEmMASmG0i0wxPq5J9mT0CB1EQy2A2p+Bra2ayaa/AdB04t89/1O/w1cDnyilFU=';
 $host = "ec2-107-22-211-182.compute-1.amazonaws.com";
 $user = "mmdkvvqziulstc";
 $pass = "e10240d71df70c411f5201bc37491e9091491ff276b8d8b66f8e507ea5b7dc22";
 $db = "dcv361109jo6fh";
-
-http_response_code(200);
+date_default_timezone_set("Asia/Bangkok");
+$date = date("Y-m-d");
+function showtime($time)
+{
+	$date = date("Y-m-d");
+	$h = split(":", $time);
+	if ($h[1] < 15)
+	{
+		$h[1] = "00";
+		$selectbydate = "select * from weatherstation where \"DATETIME\" BETWEEN '$date $h[0]:0:00' and '$date $h[0]:15:00' order by \"DATETIME\" desc limit 1";
+	}
+	else
+	if ($h[1] >= 15 && $h[1] < 30)
+	{
+		$h[1] = "15";
+		$selectbydate = "select * from weatherstation where \"DATETIME\" BETWEEN '$date $h[0]:15:01' and '$date $h[0]:30:00' order by \"DATETIME\" desc limit 1";
+	}
+	else
+	if ($h[1] >= 30 && $h[1] < 45)
+	{
+		$h[1] = "30";
+		$selectbydate = "select * from weatherstation where \"DATETIME\" BETWEEN '$date $h[0]:30:01' and '$date $h[0]:45:00' order by \"DATETIME\" desc limit 1";
+	}
+	else
+	if ($h[1] >= 45)
+	{
+		$h[1] = "45";
+		$selectbydate = "select * from weatherstation where \"DATETIME\" BETWEEN '$date $h[0]:45:01' and '$date $h[0]:59:59' order by \"DATETIME\" desc limit 1";
+	}
+	
+	return array(
+		$h[0] . ":" . $h[1],
+		$selectbydate
+	);
+}
 // database
 $dbconn = pg_connect("host=" . $GLOBALS['host'] . " port=5432 dbname=" . $GLOBALS['db'] . " user=" . $GLOBALS['user'] . " password=" . $GLOBALS['pass']) or die('Could not connect: ' . pg_last_error());
 // Get POST body content
@@ -587,7 +619,94 @@ if (!is_null($events['events']))
 				$messages = ['type' => 'text', 'text' => "https://docs.google.com/spreadsheets/d/1HKA610ClbB0WvbL24hVPY5J3llwyoTYj0ShoLDFefsQ/edit?usp=sharing"];
 			}
 				
+			//BeginCase
+			if (ereg_replace('[[:space:]]+', '', trim($text)) == "0609"){
+				
+  				$messages = ['type' => 'text', 'text' => "ปริมาตร"];
+			}
 			
+			
+			//EndCase
+			if (trim(strtoupper($text)) == "a")
+			{
+				$messages = ['type' => 'text', 'text' => "a"];
+			}
+			if (ereg_replace('[[:space:]]+', '', strtoupper($text)) == "a")
+			{
+				$messages = [
+				'type' => 'text',
+				'text' => "https://drive.google.com/open?id=14rP9TkpqLo3UwBcUzOu5zeoWu2tMp9eR"];
+			}
+			if (trim(strtoupper($text)) == "a")
+			{
+				$messages = ['type' => 'text', 'text' => "https://drive.google.com/open?id=14rP9TkpqLo3UwBcUzOu5zeoWu2tMp9eR"];
+			}
+			if ($text == "รูป")
+			{
+				$messages = ['type' => 'image', 'originalContentUrl' => "https://sv6.postjung.com/picpost/data/184/184340-1-2995.jpg", 'previewImageUrl' => "https://sv6.postjung.com/picpost/data/184/184340-1-2995.jpg"];
+			}
+			if (ereg_replace('[[:space:]]+', '', strtoupper($text)) == "info")
+			{
+				$messages = ['type' => 'text', 'text' => "ยางพาราเป็นพืชเศรฐกิจไทย" ."\n"."อ่านเพิ่มเติม: https://th.wikipedia.org/wiki/%E0%B8%A2%E0%B8%B2%E0%B8%87%E0%B8%9E%E0%B8%B2%E0%B8%A3%E0%B8%B2"];
+			}
+				
+			if ( ereg_replace('[[:space:]]+', '', trim($text)) == "O")
+			{
+				$rs = pg_query($dbconn, $sqlgetlastrecord) or die("Cannot execute query: $query\n");
+				$templink = "";
+				while ($row = pg_fetch_row($rs))
+				{
+					$templink = $row[1];
+				}
+				$messages = ['type' => 'image', 'originalContentUrl' => $templink, 'previewImageUrl' => $templink];
+			}
+			$textSplited = split(" ", $text);
+			if ( ereg_replace('[[:space:]]+', '', trim($textSplited[0])) == "O")
+			{
+				$dataFromshowtime = showtime($textSplited[1]);
+				$rs = pg_query($dbconn, $dataFromshowtime[1]) or die("Cannot execute query: $query\n");
+				$templink = ""; 
+				$qcount=0;
+				while ($row = pg_fetch_row($rs))
+				{
+					$templink = $row[1];
+					$qcount++;
+				}
+				//$messages = ['type' => 'text', 'text' => "HI $dataFromshowtime[0] \n$dataFromshowtime[1] \n$templink"
+				if ($qcount > 0){
+				$messages = [
+				'type' => 'image',
+				'originalContentUrl' => $templink,
+					'previewImageUrl' => $templink
+				];}
+				else {
+					$messages = [
+						'type' => 'image',
+						'originalContentUrl' => "https://imgur.com/aOWIijh.jpg",
+							'previewImageUrl' => "https://imgur.com/aOWIijh.jpg" 
+		
+						];
+				}
+			}
+			if ($text == "O")
+			{
+				$rs = pg_query($dbconn, $sqlgetlastrecord) or die("Cannot execute query: $query\n");
+				$templink = "";
+				while ($row = pg_fetch_row($rs))
+				{
+					$templink = $row[1];
+				}
+				$messages = ['type' => 'image', 'originalContentUrl' => $templink, 'previewImageUrl' => $templink];
+			}
+			
+			/*if($text == "image"){
+			$messages = [
+			$img_url = "http://sand.96.lt/images/q.jpg";
+			$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
+			$response = $bot->replyMessage($event->getReplyToken(), $outputText);
+			];
+			}*/
+			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = ['replyToken' => $replyToken, 'messages' => [$messages], ];
 			$post = json_encode($data);
@@ -607,4 +726,5 @@ if (!is_null($events['events']))
 		}
 	}
 }
-?>
+echo "OK";
+echo $date;
