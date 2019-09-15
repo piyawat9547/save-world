e9554d354b329475069e194df8410a999dfc624f
piyawat9547 <55186281+piyawat9547@users.noreply.github.com>
2019-09-15 20:49:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -52,6 +52,12 @@
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
+  else {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "error";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
 function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
         $ch = curl_init();
